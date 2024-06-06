29ede7792fe4ac83e906bd6502a56e90787f315c
Thomas LeBlan <t.leblan.tlb@gmail.com>
2024-06-06 22:52:18 +0200

    ajout gameplay setting au menu

--- Assets/Script/Menu/menuController.cs
+++ Assets/Script/Menu/menuController.cs
@@ -1,16 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Transactions;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using TMPro;
+using Unity.VisualScripting;
 
 public class menuController : MonoBehaviour
 {
     // Volume Setting
-    [Header("Volume Setting")]
+    [Header("Volume Settings")]
     [SerializeField] private TMP_Text volumeTextValue = null;
     [SerializeField] private Slider volumeSlider = null;
+    [SerializeField] private float defaultVolume = 1.0f;
+
+    [Header("Gameplay Settings")] 
+    [SerializeField] private TMP_Text ControllerSenTextValue = null;
+    [SerializeField] private Slider controllerSenSlider = null;
+    [SerializeField] private int defaultSen = 5;
+    public int mainControllerSen = 5;
+
+    [Header("Toggle Settings")] 
+    [SerializeField] private Toggle InvertYToggle = null;
     
     [Header("Confirmation")]
     [SerializeField] private GameObject confirmationPrompt = null;
@@ -57,6 +69,47 @@ public class menuController : MonoBehaviour
     {
         PlayerPrefs.SetFloat("masterVolume", AudioListener.volume);
         StartCoroutine(ConfirmationBox());
+    }
+
+    public void SetControllerSen(float sensitivity)
+    {
+        mainControllerSen = Mathf.RoundToInt(sensitivity);
+        ControllerSenTextValue.text = sensitivity.ToString("0");
+    }
+
+    public void GameplayApply()
+    {
+        if (InvertYToggle.isOn)
+        {
+            PlayerPrefs.SetInt("masterInvertY", 1);
+        }
+        else
+        {
+            PlayerPrefs.SetInt("masterInvertY", 0);
+        }
+        
+        PlayerPrefs.SetFloat("masterSen", mainControllerSen);
+        StartCoroutine(ConfirmationBox());
+    }
+    
+    public void ResetButton(string MenuType)
+    {
+        if (MenuType == "Audio")
+        {
+            AudioListener.volume = defaultVolume;
+            volumeSlider.value = defaultVolume;
+            volumeTextValue.text = defaultVolume.ToString("0.0");
+            VolumeApply();
+        }
+
+        if (MenuType == "Gameplay")
+        {
+            ControllerSenTextValue.text = defaultSen.ToString("0");
+            controllerSenSlider.value = defaultSen;
+            mainControllerSen = defaultSen;
+            InvertYToggle.isOn = false;
+            GameplayApply();
+        }
     }
 
     public IEnumerator ConfirmationBox()
