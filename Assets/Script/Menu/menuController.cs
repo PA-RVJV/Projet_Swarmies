e9ec7bc7cbfbb2cde0d07c9c17d05c9532395794
Thomas LeBlan <t.leblan.tlb@gmail.com>
2024-06-09 19:11:37 +0200

    update menu avec graphics setting et redesign

--- Assets/Script/Menu/menuController.cs
+++ Assets/Script/Menu/menuController.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Transactions;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 using TMPro;
 using Unity.VisualScripting;
+using UnityEngine.UIElements;
+using Slider = UnityEngine.UI.Slider;
+using Toggle = UnityEngine.UI.Toggle;
 
 public class menuController : MonoBehaviour
 {
@@ -23,6 +25,13 @@ public class menuController : MonoBehaviour
 
     [Header("Toggle Settings")] 
     [SerializeField] private Toggle InvertYToggle = null;
+
+    [Header("Graphics Settings")] 
+    [SerializeField] private TMP_Dropdown qualityDropdown;
+    [SerializeField] private Toggle fullScreenToggle;
+        
+    private int _qualityLevel;
+    private bool _isFullScreen;
     
     [Header("Confirmation")]
     [SerializeField] private GameObject confirmationPrompt = null;
@@ -32,6 +41,43 @@ public class menuController : MonoBehaviour
     public string newGameLevel;
     private string levelToLoad;
     [SerializeField] private GameObject noSavedGameDialog = null;
+
+    [Header("Resolution Dropwdowns")] 
+    public TMP_Dropdown resolutionDropdown;
+    private Resolution[] resolutions;
+    
+    // récupération des resolution
+    void Start()
+    {
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.width && resolutions[i].height == Screen.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+    
+    // setting de la resolution
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
     
     // Méthode pour charcher la scene de base
     public void NewGameDialogYes()
@@ -91,7 +137,27 @@ public class menuController : MonoBehaviour
         PlayerPrefs.SetFloat("masterSen", mainControllerSen);
         StartCoroutine(ConfirmationBox());
     }
-    
+
+    public void SetFullScreen(bool isFullscreen)
+    {
+        _isFullScreen = isFullscreen;
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        _qualityLevel = qualityIndex;
+    }
+
+    public void GraphicsApply()
+    {
+        PlayerPrefs.SetInt("masterQuality", _qualityLevel);
+        QualitySettings.SetQualityLevel(_qualityLevel);
+
+        PlayerPrefs.SetInt("masterFullscreen", (_isFullScreen ? 1 : 0));
+        Screen.fullScreen = _isFullScreen;
+
+        StartCoroutine(ConfirmationBox());
+    }
     public void ResetButton(string MenuType)
     {
         if (MenuType == "Audio")
@@ -109,6 +175,20 @@ public class menuController : MonoBehaviour
             mainControllerSen = defaultSen;
             InvertYToggle.isOn = false;
             GameplayApply();
+        }
+
+        if (MenuType == "Graphics")
+        {
+            qualityDropdown.value = 1;
+            QualitySettings.SetQualityLevel(1);
+
+            fullScreenToggle.isOn = false;
+            Screen.fullScreen = false;
+
+            Resolution currentResolution = Screen.currentResolution;
+            Screen.SetResolution(currentResolution.width, currentResolution.height, Screen.fullScreen);
+            resolutionDropdown.value = resolutions.Length;
+            GraphicsApply();
         }
     }
 
