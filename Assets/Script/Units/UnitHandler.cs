a5e594174310ba3ef31680a80513f24c26fd18c4
Meithal <hurin8888@gmail.com>
2024-06-11 14:51:08 +0200

    Ajout composant PV et Pastille minimap aux mairies

--- Assets/Script/Units/UnitHandler.cs
+++ Assets/Script/Units/UnitHandler.cs
@@ -1,9 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 using PS.Player;
+using Script.Traits;
+using UnityEngine.Assertions;
+using UnityEngine.UI;
 
 namespace PS.Units
 {
@@ -11,15 +11,24 @@ namespace PS.Units
     {        
         public PlayerManager playerManager;
         [SerializeField]
-        private Unit warrior, shooter, healer, worker;
+        private Unit warrior, shooter, healer, worker, mairie;
 
         public LayerMask pUnitLayer;
         public LayerMask eUnitLayer;
 
+        public GameObject _HealthPrefab;
+        public GameObject _MinimapPastillePrefab;
+        
         void Start()
         {
             pUnitLayer = LayerMask.NameToLayer("PlayerUnits");
             eUnitLayer = LayerMask.NameToLayer("EnemyUnits");
+
+            Assert.IsNotNull(mairie);
+            Assert.IsNotNull(worker);
+            Assert.IsNotNull(healer);
+            Assert.IsNotNull(shooter);
+            Assert.IsNotNull(warrior);
         }
 
         public UnitStatTypes.Base GetUnitStats(string type)
@@ -38,6 +47,9 @@ namespace PS.Units
                     break;
                 case "worker":
                     unit = worker;
+                    break;
+                case "mairie":
+                    unit = mairie;
                     break;
                 default:
                     Debug.Log($"Unit Type : {type} not found");
@@ -59,6 +71,8 @@ namespace PS.Units
                     string unitName = child.name.Substring(0, child.name.Length - 1).ToLower();
                     var stats = GetUnitStats(unitName);
                     
+                    Unity.Assertions.Assert.IsNotNull(stats);
+                    
                     if (type == pUnits)
                     {
                         Player.PlayerUnit pU = unit.GetComponent<Player.PlayerUnit>();
@@ -69,6 +83,38 @@ namespace PS.Units
                         Enemy.EnemyUnit eU = unit.GetComponent<Enemy.EnemyUnit>();
                         // set unit stats in each unit
                         eU.baseStats = GetUnitStats(unitName);
+                    }
+                    else
+                    {
+                        Assert.IsTrue(false);
+                    }
+                    
+                    // ajoute une barre de vie et une pastille de minimap si elle est absente
+                    if (unit.GetComponentInChildren<HealthBarTrait>() == null)
+                    {
+                        GameObject go;
+                        go=Instantiate(_HealthPrefab, unit.position, Quaternion.identity);
+                        go.transform.SetParent(unit, false);
+
+                        go.transform.localPosition = new Vector3(0, 5, 0);
+                    }
+                    
+                    // ajoute une pastille de minimap si elle est absente
+                    if (unit.GetComponentInChildren<MinimapPastilleTrait>() == null)
+                    {
+                        GameObject go;
+                        go=Instantiate(_MinimapPastillePrefab, unit.position, Quaternion.Euler(270, 0, 0));
+                        go.transform.SetParent(unit, false);
+                        go.transform.localPosition = new Vector3(0, 15, 0);
+                        //go.transform.rotation = 
+                        if (type == pUnits)
+                        {
+                            go.GetComponentInChildren<Image>().color = Color.blue;
+                        }
+                        else if(type == eUnits)
+                        {
+                            go.GetComponentInChildren<Image>().color = Color.red;
+                        }
                     }
                 }
             }
