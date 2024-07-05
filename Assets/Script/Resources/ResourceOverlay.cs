7b4fbd5fd121bd389199f84fbaccf91a4816113c
Thomas LeBlan <t.leblan.tlb@gmail.com>
2024-07-05 18:33:46 +0200

    ajout interface resource + script et prefab resourceZone

---
+++ Assets/Script/Resources/ResourceOverlay.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RessourceOverlay : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ResourcesManager();
+    }
+    
+    public void ResourcesManager()
+    {
+        if (gameObject.name == "WoodBackground")
+        {
+            
+        }
+
+        if (gameObject.name == "StoneBackground")
+        {
+            
+        }
+    }
+}
---
+++ Assets/Script/Resources/ResourceZone.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ResourceType
+{
+    Wood,
+    Stone,
+    Iron
+}
+
+public class ResourceZone : MonoBehaviour
+{
+    public ResourceType resourceType;
+    public int resourceAmount = 100;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+
